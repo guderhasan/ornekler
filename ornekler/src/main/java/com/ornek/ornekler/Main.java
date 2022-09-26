b97bc8ad1132dd97ba987e5ac4c795ee0a6bad84
guderhasan <68234046+guderhasan@users.noreply.github.com>
2022-09-26 19:45:42 +0300

    syser yazdım

--- ornekler/src/main/java/com/ornek/ornekler/Main.java
+++ ornekler/src/main/java/com/ornek/ornekler/Main.java
@@ -8,22 +8,22 @@ import io.dropwizard.setup.Environment;
 
 public class Main extends Application<ServiceConfiguration> {
 
-    public static void main(String[] args) throws Exception {
-        new Main().run(args);
-    }
+	public static void main(String[] args) throws Exception {
+		System.err.println("wefwefwe");
+		new Main().run(args);
+	}
 
-    @Override
-    public String getName() {
-        return "ornekler";
-    }
+	@Override
+	public String getName() {
+		return "ornekler";
+	}
 
-    @Override
-    public void initialize(Bootstrap<ServiceConfiguration> bootstrap) {
-    }
+	@Override
+	public void initialize(Bootstrap<ServiceConfiguration> bootstrap) {
+	}
 
-    @Override
-    public void run(ServiceConfiguration cfg,
-                    Environment env) throws Exception {
-        env.jersey().register(HealthCheckResource.class);
-    }
+	@Override
+	public void run(ServiceConfiguration cfg, Environment env) throws Exception {
+		env.jersey().register(HealthCheckResource.class);
+	}
 }
